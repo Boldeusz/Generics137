a37ba575d59562bede25a19d922b61c6136fd7d2
Boldeusz <mnpower@poczta.onet.pl>
2021-03-02 12:21:07 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,10 +10,43 @@ public class Main {
 	BaseballPlayer pat = new BaseballPlayer("Pat");
 	SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
-	Team someTeam = new Team("Warsaw");
+	Team<FootballPlayer> someTeam = new Team<>("Warsaw");
 	someTeam.addPlayer(joe);
-	someTeam.addPlayer(pat);
-	someTeam.addPlayer(beckham);
+//	someTeam.addPlayer(pat);
+//	someTeam.addPlayer(beckham);
         System.out.println(someTeam.numPlayers());
+
+        Team<BaseballPlayer> baseballPlayerTeam = new Team<>("Lublin");
+        baseballPlayerTeam.addPlayer(pat);
+
+		Team<SoccerPlayer> soccerPlayerTeam = new Team<>("Chełm");
+		soccerPlayerTeam.addPlayer(beckham);
+
+		Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+		FootballPlayer banks = new FootballPlayer("Gordon");
+		melbourne.addPlayer(banks);
+
+		Team<FootballPlayer> hawthorn= new Team<>("Hawthorn");
+		Team<FootballPlayer> fremantle= new Team<>("Fremantle");
+
+		hawthorn.matchResult(fremantle, 1, 0);
+		hawthorn.matchResult(someTeam, 3, 8);
+
+		someTeam.matchResult(fremantle, 2, 1);
+//		someTeam.matchResult(baseballPlayerTeam, 1, 1);
+
+		System.out.println("Rankings");
+		System.out.println(someTeam.getName() + ": " + someTeam.ranking());
+		System.out.println(melbourne.getName() + ": " + melbourne.ranking());
+		System.out.println(fremantle.getName() + ": " + fremantle.ranking());
+		System.out.println(hawthorn.getName() + ": " + hawthorn.ranking());
+
+		System.out.println(someTeam.compareTo(melbourne));
+		System.out.println(someTeam.compareTo(hawthorn));
+		System.out.println(hawthorn.compareTo(someTeam));
+		System.out.println(melbourne.compareTo(fremantle));
+
+//		ArrayList<Team> teams;
+//		Collections.sort(teams);
     }
 }
